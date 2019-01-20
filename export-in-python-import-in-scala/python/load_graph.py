e5eef5467895c90b0fbccd749bad725cc90bed7f
chenqiang <fucus@qq.com>
2019-01-20 22:10:18 +0800

    test saved pd file ;

---
+++ export-in-python-import-in-scala/python/load_graph.py
@@ -0,0 +1,49 @@
+import argparse 
+import tensorflow as tf
+
+
+def load_graph(frozen_graph_filename):
+    # We load the protobuf file from the disk and parse it to retrieve the 
+    # unserialized graph_def
+    with tf.gfile.GFile(frozen_graph_filename, "rb") as f:
+        graph_def = tf.GraphDef()
+        graph_def.ParseFromString(f.read())
+
+    # Then, we import the graph_def into a new Graph and returns it 
+    with tf.Graph().as_default() as graph:
+        # The name var will prefix every op/nodes in your graph
+        # Since we load everything in a new graph, this is not needed
+        tf.import_graph_def(graph_def, name="prefix")
+    return graph
+
+if __name__ == '__main__':
+    (train_images, train_labels), (test_images, test_labels) = tf.keras.datasets.mnist.load_data()
+
+    train_labels = train_labels[:1000]
+    test_labels = test_labels[:1000]
+
+    train_images = train_images[:1000].reshape(-1, 28 * 28) / 255.0
+    test_images = test_images[:1000].reshape(-1, 28 * 28) / 255.0
+
+    # Let's allow the user to pass the filename as an argument
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--frozen_model_filename", default="results/frozen_model.pb", type=str, help="Frozen model file to import")
+    args = parser.parse_args()
+
+    # We use our "load_graph" function
+    graph = load_graph(args.frozen_model_filename)
+        
+    # We access the input and output nodes 
+    x = graph.get_tensor_by_name('prefix/dense_2_input:0')
+    y = graph.get_tensor_by_name('prefix/dense_3/Softmax:0')
+        
+    # We launch a Session
+    with tf.Session(graph=graph) as sess:
+        # Note: we don't nee to initialize/restore anything
+        # There is no Variables in this graph, only hardcoded constants 
+        y_out = sess.run(y, feed_dict={
+            x: [range(28*28)] # < 45
+        })
+        # I taught a neural net to recognise when a sum of numbers is bigger than 45
+        # it should return False in this case
+        print(y_out) # [[ False ]] Yay, it works!
